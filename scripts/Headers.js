ffb0464f53eeac74fad9eef1335102a04967ee38
AntonLushchay <av.lushchay17@gmail.com>
2025-03-01 18:32:06 +0100

    header layout

---
+++ scripts/Headers.js
@@ -0,0 +1,11 @@
+class header {
+    selectors = {
+        root: '[data-js-header]',
+        overlay: '[data-js-header-overlay]',
+        burgerButton: '[data-js-header-burger-button]',
+    };
+
+    stateClasses = {};
+}
+
+export default header;
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import Header from './Headers.js';
+
+new Header();
